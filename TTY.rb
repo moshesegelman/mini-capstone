6651667494c8cce8d9de24625cf7d16e43b39fbb
moshesegelman <moshesegelman41@gmail.com>
2020-05-27 14:18:19 -0400

    adds product 2

---
+++ TTY.rb
@@ -0,0 +1,2 @@
+require 
+table = TTY::Table.new ['header1','header2'], [['a1', 'a2'], ['b1', 'b2']]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-# product = Product.new(name: "Gucci Slides", price: 10, image_url:"https://cache.mrporter.com/variants/images/666467151987779/fr/w2000_q80.jpg", description: "Definitley not real for 10 bucks")
-# product.save
+product1 = Product.new(name: "Gucci Slides", price: 10, image_url:"https://cache.mrporter.com/variants/images/666467151987779/fr/w2000_q80.jpg", description: "Definitley not real for 10 bucks")
+
+
+product2 = Product.new(name: "Football", price: 12, image_url:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTyjfbjaSlxKbib_nvC0nhXRIjlI_6-1SOtegRrXE4ZWvNQ3il3LbL8_8ZfoH946whxKfk6C8TC59g&usqp=CAc", description: "More expensive then the Gucci slides!")
+
