3d452caae2392b14b07554a3cbc635cb3c29878b
moshesegelman <moshesegelman41@gmail.com>
2020-05-27 12:59:08 -0400

    adds product 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# product = Product.new(name: "Gucci Slides", price: 10, image_url:"https://cache.mrporter.com/variants/images/666467151987779/fr/w2000_q80.jpg", description: "Definitley not real for 10 bucks")
+# product.save
