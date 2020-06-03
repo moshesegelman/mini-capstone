275d8991adfd22d8a60abc37d9979f5237c171bc
moshesegelman <moshesegelman41@gmail.com>
2020-06-03 12:59:10 -0400

    adds partial

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,15 @@
 class Product < ApplicationRecord
 
   def is_discounted?
-    if price < 10
-      return true
-    else 
-      return false
-    end
+    price < 10
   end
 
   def tax
-    @tax = price * 0.09
+    price * 0.09
   end
 
   def total
-    price + @tax
+    price + tax
   end
 
 end
